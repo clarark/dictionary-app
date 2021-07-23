bc2f68f8cf0a291448c0fc73ab4c4794c66b17bb
clarark <clara.rk@hotmail.com>
2021-07-23 15:59:37 -0300

    added a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         Dictionary
+        <a href="/" className="btn btn-primary">
+          hello
+        </a>
       </header>
     </div>
   );
